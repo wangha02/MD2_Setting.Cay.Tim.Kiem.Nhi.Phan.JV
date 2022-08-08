8c535b8529c8eb668d29b13aa14e69a34f8a2ef3
unknown <btkkentxx2002@gmail.com>
2022-08-08 22:55:17 +0700

    jv

---
+++ src/rikkei/academy/AbstractTree.java
@@ -0,0 +1,8 @@
+package rikkei.academy;
+
+public abstract class AbstractTree<E> implements Tree<E>{
+    @Override
+    /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+}
---
+++ src/rikkei/academy/Tree.java
@@ -0,0 +1,19 @@
+package rikkei.academy;
+
+public interface Tree<E> {
+    /**
+     * Insert element e into the binary search tree.
+     * Return true if the element is inserted successfully.
+     */
+    boolean insert(E e);
+
+    /**
+     * Inorder traversal from the root
+     */
+    void inorder();
+
+    /**
+     * Get the number of nodes in the tree
+     */
+    int getSize();
+}
